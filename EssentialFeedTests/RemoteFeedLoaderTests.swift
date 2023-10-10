7a667a01a73fe8883300b42fc1fff34d7ea65de3
ioanna <anasioan@yahoo.gr>
2023-10-10 10:50:11 +0300

    Request data from URL on "load"

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,21 +8,33 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
         let sut = RemoteFeedLoader()
         
-        //first test: We did not make url request since that should only happen when .load() is invoked
-        XCTAssertNil(client.requestedURL)
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
@@ -38,6 +50,22 @@ class RemoteFeedLoaderTests: XCTestCase {
  Collaborator: Like Alamofire, URLSession, HttpClient
  
  XCTAssert: asserts that an expression is true
+ 
+ test_init_doesNotRequestDataFromURL():
+ first test - We did not make url request since that should only happen when .load() is invoked
+ 
+ test_load_requestDataFromURL():
+ second test -  in this test is expected to call load() from RemoteFeedLoader and get the data from the internet.
+ We can pass client to RemoteFeedLoader fwith dependency injection(
+    Constructor Dependency ijection:   let sut = RemoteFeedLoader(client: client)
+    Property dependency injection: sut.client = client
+    method dependency injection: sut.load(client: client)
+    )
+ 
+But instructors suggest not dependency injection but a more concrete way to communicate RemoteFeedLoader and HttpClient, Singleton Impementation.
+ 
+ The test are passed, but why to have Singleton HttpClient and not have as many HttpClients as i want?
+ 
  
  */
 
